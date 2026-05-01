7c8548464a22595a488a4b8530a76e57c44d5577
wienni112 <55889937+wienni112@users.noreply.github.com>
2026-05-01 22:36:30 +0200

    Update run.sh

--- cs-routeros-bouncer/run.sh
+++ cs-routeros-bouncer/run.sh
@@ -1,10 +1,17 @@
-#!/usr/bin/env sh
+#!/usr/bin/with-contenv bashio
+
 set -e
 
-export CROWDSEC_URL="${CROWDSEC_URL:-$(bashio::config 'crowdsec_url')}"
-export CROWDSEC_BOUNCER_API_KEY="${CROWDSEC_BOUNCER_API_KEY:-$(bashio::config 'crowdsec_bouncer_api_key')}"
-export MIKROTIK_HOST="${MIKROTIK_HOST:-$(bashio::config 'mikrotik_host')}"
-export MIKROTIK_USER="${MIKROTIK_USER:-$(bashio::config 'mikrotik_user')}"
-export MIKROTIK_PASS="${MIKROTIK_PASS:-$(bashio::config 'mikrotik_pass')}"
+export CROWDSEC_URL="$(bashio::config 'crowdsec_url')"
+export CROWDSEC_BOUNCER_API_KEY="$(bashio::config 'crowdsec_bouncer_api_key')"
+export MIKROTIK_HOST="$(bashio::config 'mikrotik_host')"
+export MIKROTIK_USER="$(bashio::config 'mikrotik_user')"
+export MIKROTIK_PASS="$(bashio::config 'mikrotik_pass')"
+export MIKROTIK_TLS="$(bashio::config 'mikrotik_tls')"
+export LOG_LEVEL="$(bashio::config 'log_level')"
 
-exec /app/cs-routeros-bouncer
+bashio::log.info "Starting cs-routeros-bouncer"
+bashio::log.info "CrowdSec URL: ${CROWDSEC_URL}"
+bashio::log.info "MikroTik Host: ${MIKROTIK_HOST}"
+
+exec /usr/local/bin/cs-routeros-bouncer
